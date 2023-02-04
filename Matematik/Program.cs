0bf041d3c2968857e03ae678580c14d1ab3c53d4
SAMET PC <SAMET PC@DESKTOP-H1PNMFB>
2023-02-04 17:13:58 +0300

    Proje dosyası ekle.

---
+++ Matematik/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Matematik
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DortIslem dortIslem= new DortIslem();
+            dortIslem.Topla(20, 11);
+            dortIslem.Topla(2, 5);    
+        }
+    }
+}
---
+++ metetlar/SepetManeger.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace metetlar
+{
+    internal class SepetManeger
+    {
+        public void Ekle() 
+        {
+            Console.WriteLine("Sepete eklendi");  
+            
+        }
+    }
+}
